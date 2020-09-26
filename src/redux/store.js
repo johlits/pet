30453f1246661dc3583965e9d590f029814a74fe
Johan <johlits@palplanner.com>
2020-09-26 12:38:21 +0200

    Add persist to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
 import rootReducer from "./reducers";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ &&window.__REDUX_DEVTOOLS_EXTENSION__() // Redux DevTools, a must
 
-export default createStore(rootReducer, enhancer);
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer, enhancer)
+export const persistor = persistStore(store);
